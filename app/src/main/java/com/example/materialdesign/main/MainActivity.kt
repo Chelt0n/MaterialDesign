c94c41f7cbf9253dd91ac5597748b87596e7faf0
Chelt0n <qys1@yandex.ru>
2021-12-23 10:32:26 +0300

    2 урок завершен

--- app/src/main/java/com/example/materialdesign/main/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/main/MainActivity.kt
@@ -1,26 +1,30 @@
 package com.example.materialdesign.main
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.materialdesign.FontTypes
 import com.example.materialdesign.view.DailyImageFragment
 import com.example.materialdesign.R
+import com.example.materialdesign.SharedPref
+import com.example.materialdesign.SharedPref.Companion.SETTINGS
+import com.example.materialdesign.Theme
 import com.example.materialdesign.view.BottomNavigationDrawerFragment
-import com.example.materialdesign.view.ChipsFragment
+import com.example.materialdesign.view.SettingsFragment
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var sharedPref: SharedPref
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPref = SharedPref(getSharedPreferences(SETTINGS, MODE_PRIVATE))
         setContentView(R.layout.main_activity)
         bottomBar()
 
@@ -31,9 +35,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        setUserTheme()
+        Log.d("mylog", sharedPref.getCustomTheme().toString())
+    }
+
     private fun bottomBar() {
         val bottomAppBar: BottomAppBar = findViewById(R.id.bottom_app_bar)
-        val fab:FloatingActionButton = findViewById(R.id.fab)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         bottomAppBar.inflateMenu(R.menu.menu_item_bottom_app_bar)
         setSupportActionBar(bottomAppBar)
         fab.setOnClickListener {
@@ -55,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             R.id.app_bar_settings -> {
                 supportFragmentManager.beginTransaction().replace(
                     R.id.container,
-                    ChipsFragment.newInstance()
+                    SettingsFragment.newInstance()
                 ).addToBackStack(null).commit()
             }
             android.R.id.home -> {
@@ -64,6 +74,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+     fun setUserTheme() {
+        when (sharedPref.getCustomTheme()) {
+            Theme.DAY -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            Theme.NIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+         when (sharedPref.getFontType()) {
+             FontTypes.CURSIVE -> setTheme(R.style.cursive)
+             FontTypes.SANS_SERIF_THIN -> setTheme(R.style.sans_serif)
+             FontTypes.CONDENSED -> setTheme(R.style.condensed)
+         }
     }
 
 }
--- app/src/main/java/com/example/materialdesign/view/DailyImageFragment.kt
+++ app/src/main/java/com/example/materialdesign/view/DailyImageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.materialdesign.view
 
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.IntentCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
@@ -19,11 +21,7 @@ import com.example.materialdesign.viewmodel.DailyImageViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-
-
-
-
-
+import kotlin.system.exitProcess
 
 class DailyImageFragment : Fragment() {
 
@@ -62,14 +60,14 @@ class DailyImageFragment : Fragment() {
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
 
 
-        inputLayout.setOnClickListener {
+        inputLayout.setEndIconOnClickListener() {
             val intent = Intent(Intent.ACTION_VIEW)
             val url = "https://en.wikipedia.org/wiki/${inputEditText.text.toString()}"
             val uri = Uri.parse(url)
             intent.data = uri
             startActivity(intent)
-            }
         }
+    }
 
     private fun initView(view: View) {
         dailyImageView = view.findViewById(R.id.image_view_nasa_image)
@@ -85,10 +83,11 @@ class DailyImageFragment : Fragment() {
     private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
-        btnDescription.setOnClickListener{
+        btnDescription.setOnClickListener {
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         }
     }
+
 
     private fun renderData(appState: AppState) {
         when (appState) {
@@ -96,7 +95,7 @@ class DailyImageFragment : Fragment() {
                 val serverResponseData = appState.serverResponseData
                 val url = serverResponseData.url
                 if (url.isNullOrEmpty()) {
-                    Toast.makeText(context,"Ошибка",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Ошибка", Toast.LENGTH_SHORT).show()
                 } else {
                     dailyImageView.load(url) {
                         lifecycle(this@DailyImageFragment)
@@ -107,14 +106,16 @@ class DailyImageFragment : Fragment() {
                 }
             }
             is AppState.Loading -> {
-                Toast.makeText(context,"Загрузка",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Загрузка", Toast.LENGTH_SHORT).show()
             }
             is AppState.Error -> {
-                Toast.makeText(context,"Ошибка",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "Ошибка", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
-    companion object{
+
+    companion object {
         fun newInstance() = DailyImageFragment()
     }
 }
